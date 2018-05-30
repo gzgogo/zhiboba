6546eabad213ed112276da32670b780262aafaad
gongzhen.gz <gongzhen.gz@alibaba-inc.com>
2018-05-31 01:02:08 +0800

    finished

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,8 +6,11 @@ Page({
   data: {
     currentType: null,
     gameTypes: [{
-      key: 'nba',
+      key: 'NBA',
       name: 'NBA'
+    }, {
+      key: '世界杯',
+      name: '世界杯'
     }, {
       key: 'basketball',
       name: '篮球'
@@ -18,20 +21,40 @@ Page({
       key: 'all',
       name: '全部'
     }],
-    lives: []
+    lives: [],
+    liveMap: {}
   },
+
   onLoad: function (options) {
-    var self = this;
     var lastType = wx.getStorageSync(LAST_TYPE_KEY);
     this.setData({
       currentType: lastType || this.data.gameTypes[0]
     });
 
-    // wx.getStorage('lastType');
-    console.log('last type:', ); 
-    console.log('current type:', this.data.currentType.key); 
+    this.fetchData();
+  },
 
-    // 页面初始化 options为页面跳转所带来的参数
+  onPullDownRefresh: function() {
+    this.fetchData(function() {
+      wx.stopPullDownRefresh();
+    });
+  },
+
+  onTypeChange: function(e) {
+    var gameType = e.detail;
+    this.setData({
+      currentType: gameType
+    });
+    wx.setStorageSync(LAST_TYPE_KEY, gameType);
+  },
+
+  onGameTap: function(e) {
+    console.log('on game tap: ', e.currentTarget.dataset.url);
+  },
+
+  fetchData: function(cb) {
+    var self = this;
+    
     wx.request({
       url: 'https://120.55.23.46/json/zhibo/saishi.htm?device=iPhone%206s%20Plus&_only_care=3&version_code=4.6.7&os=iOS&UDID=caa6e30097c2067f4d67217f332486d19ce54a27&platform=ios&os_version=11.3.1&appname=zhibo8&_platform=ios&IDFA=EA95F362-3E46-4B71-BB16-E4E20F7A2C7B&pk=com.zhibo8.pro', //仅为示例，并非真实的接口地址
       header: {
@@ -39,19 +62,51 @@ Page({
       },
       success: function (res) {
         console.log(res.data);
-        self.setData({
-          lives: res.data
-        })
+        self.extract(res.data);
+
+        typeof cb === 'function' && cb();
       }
     })
   },
-  onTypeChange: function(e) {
-    var gameType = e.detail;
-    wx.setStorageSync(LAST_TYPE_KEY, gameType);
-    console.log('gameType: %o', gameType);
+
+  extract(data) {
+    var self = this;
+    var map = {};    
+    var gameTypes = this.data.gameTypes;
+
+    gameTypes.forEach(function(gameType) {
+      map[gameType.key] = self.extractByType(data, gameType);
+    });
+
+    console.log('map: %o', map);
+    this.setData({
+      livesMap: map
+    });
   },
 
-  onGameTap: function(e) {
-    console.log('on game tap: ', e.currentTarget.dataset.url);
+  extractByType(data, gameType) {
+    var typeData = [];
+
+    if (Array.isArray(data)) {
+      data.forEach(function(item) {
+        var live = {
+          formatDate: item.formatDate,
+          date: item.date,
+          list: []
+        };
+
+        item.list.forEach(function(game) {
+          if ([game.type, game.label].join().indexOf(gameType.key) > -1) {
+            live.list.push(game)
+          }
+        })
+
+        if (live.list.length > 0) {
+          typeData.push(live);
+        }
+      })
+    }
+
+    return typeData;
   }
 })
